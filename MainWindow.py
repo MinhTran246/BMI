76289477085db91a4b57cc30592ff16c959b1107
hoangvy10d <nguyenhoangvy0545@gmail.com>
2024-11-06 12:30:28 +0700

    Update MainWindow.py

--- MainWindow.py
+++ MainWindow.py
@@ -108,3 +108,13 @@ class Ui_MainWindow(object):
         self.label_5.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\">Comment:</p></body></html>"))
         self.pushButtonCalculate.setText(_translate("MainWindow", "Calculate"))
         self.pushButtonClose.setText(_translate("MainWindow", "Close"))
+        from PyQt6.QtWidgets import QApplication, QMainWindow
+
+from CHAP7.LearnQPushButton.UI.MainwindowExt import MainwindowExt
+
+app=QApplication([])
+mainwindow=QMainWindow()
+myui=MainwindowExt()
+myui.setupUi(mainwindow)
+myui.showWindow()
+app.exec()
